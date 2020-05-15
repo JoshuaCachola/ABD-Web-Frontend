8c713de4333a33400cf61bf17b4fcdb29330d89c
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-05-15 11:24:43 -0700

    fix route

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ const api = {
   url:
     process.env.NODE_ENV === "development"
       ? process.env.REACT_APP_DEV_API || "http ://localhost:8008/api/v1"
-      : process.env.REACT_APP_PROD_API || "https://already-been-done.herokuapp.com",
+      : process.env.REACT_APP_PROD_API || "https://already-been-done.herokuapp.com/api/v1",
 };
 
 export default api;
