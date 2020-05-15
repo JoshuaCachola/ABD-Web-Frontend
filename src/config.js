ca9831e96e6ff0aa2a1913fc73c9545288261a60
Joshua Cachola <joshuaelliscachola@gmail.com>
2020-05-15 11:13:46 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const api = {
   url:
-    process.env.NODE_DEV === "development"
-      ? process.env.DEV_API
-      : process.env.PROD_API,
+    process.env.NODE_ENV === "development"
+      ? process.env.REACT_APP_DEV_API || "http ://localhost:8008/api/v1"
+      : process.env.REACT_APP_PROD_API || "https://already-been-done.herokuapp.com",
 };
 
 export default api;
